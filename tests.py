e054ffa75fa7c68a9dccfd22e1642dbfcd32fe60
Alex Kovtun <sirMelipharo@gmail.com>
2017-08-01 22:33:44 +0700

    code deduplication: two test cases merged into one

--- tests.py
+++ tests.py
@@ -2,6 +2,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
 from group import Group
+from contact import Contact
 
 def is_alert_present(wd):
     try:
@@ -10,7 +11,7 @@ def is_alert_present(wd):
     except:
         return False
 
-class test_add_group(unittest.TestCase):
+class tests(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver(
             firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe"
@@ -33,15 +34,54 @@ class test_add_group(unittest.TestCase):
         self.return_to_group_page()
         self.logout()
 
-    def logout(self):
-        self.wd.find_element_by_link_text("Logout").click()
+    def test_add_contact(self):
+        self.open_addressbook()
+        self.login(username="admin", password="secret")
+        self.add_contact(Contact("John", "Doe", "test Co", "322223", "test note"))
+        self.return_to_contacts_page()
+        self.logout()
+
+    def test_add_empty_contact(self):
+        self.open_addressbook()
+        self.login(username="admin", password="secret")
+        self.add_contact(Contact("", "", "", "", ""))
+        self.return_to_contacts_page()
+        self.logout()
+
+    def add_contact(self, contact):
+        wd = self.wd
+        # click 'add new'
+        wd.find_element_by_link_text("add new").click()
+        # fill contact data
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_tel)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact.note)
+        # click 'enter'
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def return_to_group_page(self):
         self.wd.find_element_by_link_text("group page").click()
 
+    def return_to_contacts_page(self):
+        self.wd.find_element_by_xpath("//div/div[4]/div/i/a[2]").click()
+
     def create_group(self, group):
         wd = self.wd
+        # click 'new group'
         wd.find_element_by_name("new").click()
+        # enter group data
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
@@ -52,6 +92,7 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # click 'submit'
         wd.find_element_by_name("submit").click()
 
     def open_groups(self):
@@ -67,6 +108,9 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def logout(self):
+        self.wd.find_element_by_link_text("Logout").click()
 
     def open_addressbook(self):
         self.wd.get("http://localhost/addressbook/")
