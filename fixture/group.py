10b3d24f27d78dc85c1c8c1b4fe15cf5913adb35
Alex Kovtun <sirMelipharo@gmail.com>
2017-08-06 20:47:10 +0700

    fixture move out from tests

--- fixture/group.py
+++ fixture/group.py
@@ -25,5 +25,15 @@ class GroupHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # click 'delete'
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
+
     def return_to_groups_page(self):
         self.app.wd.find_element_by_link_text("group page").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
-import pytest
-
-from fixture import Application
 from model import Contact
 
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_contact(app):
     app.session.login(username="admin", password="secret")
     app.contact.create(Contact("John", "Doe", "test Co", "322223", "test note"))
     app.session.logout()
+
 
 def test_add_empty_contact(app):
     app.session.login(username="admin", password="secret")
