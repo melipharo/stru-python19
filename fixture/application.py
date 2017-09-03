26768b21ad14f504bd06794f94fc691634dfd37e
Alex Kovtun <sirMelipharo@gmail.com>
2017-09-03 11:28:01 +0700

    commandline options added

--- fixture/application.py
+++ fixture/application.py
@@ -1,19 +1,29 @@
-from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium import webdriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contact import ContactHelper
 
 class Application:
-    def __init__(self):
-        self.wd = WebDriver(
-            firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe"
-        )
+    def __init__(self, browser, base_url):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox(
+                firefox_binary="C:\\Program Files\\Mozilla Firefox\\firefox.exe"
+            )
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unknown browser {}".format(browser))
+
+        self.base_url = base_url
+
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
 
     def open_addressbook(self):
-        self.wd.get("http://localhost/addressbook/")
+        self.wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
--- test/conftest.py
+++ test/conftest.py
@@ -4,14 +4,20 @@ from fixture import Application
 fixture = None
 
 @pytest.fixture
-def app():
+def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
+    username = request.config.getoption("--username")
+    password = request.config.getoption("--password")
+
     if fixture is None:
-        fixture = Application()
+        fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid():
-            fixture = Application()
-    fixture.session.ensure_login(username="admin", password="secret")
+            fixture = Application(browser=browser, base_url=base_url)
+
+    fixture.session.ensure_login(username=username, password=password)
     return fixture
 
 
@@ -23,3 +29,10 @@ def stop(request):
         fixture.destroy()
     request.addfinalizer(fin)
     return fixture
+
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="firefox")
+    parser.addoption("--baseUrl", action="store", default="http://localhost/addressbook/")
+    parser.addoption("--username", action="store", default="admin")
+    parser.addoption("--password", action="store", default="")
