4f7e6ee570c4cbee27acea485ed7eae89ffa6699
Alex Kovtun <sirMelipharo@gmail.com>
2017-10-01 16:30:12 +0700

    allure first steps

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,13 +1,17 @@
+import allure
 
 def test_add_group(app, db, check_ui, json_groups):
     group = json_groups
 
-    old_groups = db.get_group_list()
-    app.group.create(group)
-    old_groups.append(group)
+    with allure.step("Given a group list"):
+        old_groups = db.get_group_list()
 
-    new_groups = db.get_group_list()
-    assert sorted(old_groups) == sorted(new_groups)
+    with allure.step("When I add the group {} to the list".format(group)):
+        app.group.create(group)
 
-    if check_ui:
-        assert sorted(new_groups) == sorted(app.group.get_group_list())
+    with allure.step("Then the new group list is equal to the old list with the added group"):
+        old_groups.append(group)
+        new_groups = db.get_group_list()
+        assert sorted(old_groups) == sorted(new_groups)
+        if check_ui:
+            assert sorted(new_groups) == sorted(app.group.get_group_list())
